e9c2a0667b17a6a140a645d0590818ff79fbc634
evelynlacerda <evelyndslacerda@gmail.com>
2023-04-14 13:26:24 -0300

    Script refactored

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+const projectImages = document.querySelectorAll('.projetos__img')
+const arrowNext = document.getElementById('btn-next')
+const arrowBack = document.getElementById('btn-back')
+let actualImage = 0
+
+function imageOff() {
+    projectImages.forEach(image => {
+        image.classList.remove('show')
+    })
+}
+
+function imageOn() {
+    projectImages[actualImage].classList.add('show')
+}
+
+arrowNext.addEventListener('click', function () {
+    if(actualImage !== projectImages.length - 1) {
+        actualImage++
+    } else {
+        actualImage = 0
+    }
+    
+    imageOff()
+    imageOn()
+})
+
+arrowBack.addEventListener('click', function () {
+    if(actualImage !== 0) {
+        actualImage--
+    } else {
+        actualImage = projectImages.length - 1
+    }
+
+    imageOff()
+    imageOn()
+})
